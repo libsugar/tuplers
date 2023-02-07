d7626cc2f7e1b2cb880e4d3a620b6317602eddaa
U2A5F <U2A5F@outlook.com>
2023-02-07 19:26:22 +0800

    test and readme

--- tuples/src/split/split_parts.rs
+++ tuples/src/split/split_parts.rs
@@ -1,3 +1,16 @@
 //! Split into n parts
 
 include!("../gen/split_parts.rs");
+
+#[test]
+fn test() {
+    let t = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+    let a = t.split_2_parts();
+    assert_eq!(a, ((0, 1, 2, 3, 4), (5, 6, 7, 8, 9)));
+    let b = t.split_3_parts();
+    assert_eq!(b, ((0, 1, 2, 3), (4, 5, 6), (7, 8, 9)));
+    let c = t.split_4_parts();
+    assert_eq!(c, ((0, 1, 2), (3, 4, 5), (6, 7), (8, 9)));
+    let d = t.split_5_parts();
+    assert_eq!(d, ((0, 1), (2, 3), (4, 5), (6, 7), (8, 9)));
+}
